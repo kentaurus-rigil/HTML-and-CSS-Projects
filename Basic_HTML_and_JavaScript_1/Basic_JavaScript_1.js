b9852ed24954f551837d997c93ad024589efe73e
kentaurus-rigil <66184504+kentaurus-rigil@users.noreply.github.com>
2020-06-03 11:33:35 -0700

    Pushing HTML assignment page 127

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,9 @@
+
+
+alert("Hello, World");
+function My_First_Function() {
+		var string = "Kiss me, I'm Irish!";
+		var result = string.fontcolor("green");
+		document.getElementById("Irish").innerHTML = result;
+	}
+
