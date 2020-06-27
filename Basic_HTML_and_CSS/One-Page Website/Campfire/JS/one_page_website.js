1640991cdbd22e44110602a4f20d7a871a66e56b
kentaurus-rigil <66184504+kentaurus-rigil@users.noreply.github.com>
2020-06-26 17:33:32 -0700

    update to troubleshooting

--- Basic_HTML_and_CSS/One-Page Website/Campfire/JS/one_page_website.js
+++ Basic_HTML_and_CSS/One-Page Website/Campfire/JS/one_page_website.js
@@ -9,6 +9,7 @@ function closeModal() {
 }
 
 
+
 //Next/previous controls
 function plusSlides(n) {
 	showSlides(slideIndex += n);
@@ -25,7 +26,8 @@ function showSlides(n) {
 	var dots = document.getElementsByClassName("demo");
 	var captionText = document.getElementById("caption");
 	if (n > slides.length) {slideIndex = 1}
-	if (n < 1) {slideIndex = slides.length
+	if (n < 1) {slideIndex = slides.length}
+	for (i = 0; i < slides.length; i++) {
 		slides[i].style.display = "none";
 	}
 	for (i = 0; i < dots.length; i++) {
